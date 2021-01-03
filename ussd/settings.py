9abaad3f3c6b9e96f8dd31d6851c5f6f3f5e069f
BenIraa <iradukundacyuzo50@gmail.com>
2021-01-03 11:12:45 +0200

    final pro

--- ussd/settings.py
+++ ussd/settings.py
@@ -135,8 +135,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
